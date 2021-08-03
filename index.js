66efd93a0e4701d4d394eccbe3d56364d6618272
jgumerove <josh.gumerove@gmail.com>
2021-08-03 19:21:06 -0400

    commit tree 37819f4a6b65f49d2cbc04938a3940205dc4728a

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const cards = [ 'Lisa', 'Kaitlin', 'Jan' ];
+const string = "surprise"
+function writeCards(array, string) {
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++) {
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+writeCards(cards, string);
+
+function countDown(number){
+    while (number >= 0){
+    console.log(number--)
+    }
+}
